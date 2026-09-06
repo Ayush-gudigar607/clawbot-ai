57b3bd579d2e5b6794645d156a653169baab66d4
unknown <23ai009@sitmng.ac.in>
2026-09-06 21:30:49 +0530

    add the index.ts

---
+++ index.ts
@@ -0,0 +1,13 @@
+import {Command} from "commander"
+import { runwakeup } from "./terminalui/wakeup";
+
+const program=new Command();
+
+program.name("clawbot-ai").description("A CLI tool for Clawbot AI").version("0.1.0");
+
+
+program.command("wakeup").description("Wake up the Clawbot AI").action(async ()=>{
+   await runwakeup();
+});
+
+program.parseAsync(process.argv);
