5cf081a996be84b826810f802502e6a5617bad79
unknown <23ai009@sitmng.ac.in>
2026-09-07 22:30:39 +0530

    add the cli.ts which is about modes

---
+++ modes/cli.ts
@@ -0,0 +1,42 @@
+import chalk from "chalk";
+import { select, isCancel } from "@clack/prompts";
+
+export async function runcliMode() {
+  while (true) {
+    const mode = await select({
+      message: "Choose sub-cli Mode",
+      options: [
+        { value: "agent", label: "Agent Mode" },
+        { value: "plan", label: "Plan Mode" },
+        { value: "ask", label: "Ask Mode" },
+        { value: "Back", label: "Back to main menu" },
+      ],
+    });
+
+    if(isCancel(mode) || mode=="Back")
+    {
+        return;
+    }
+
+    if(mode=="agent")
+    {
+        console.log("agent");
+    }
+
+    if(mode=="plan")
+    {
+        console.log("plan")
+    }
+
+    if(mode=="ask")
+    {
+        console.log("ask")
+    }
+    
+    if(mode!=='agent' && mode!=='plan' && mode!=='ask')
+        {
+            console.log(chalk.yellow("\n That mode is not implemented yet.\n"));
+            // console.log("")
+        }
+  }
+}
