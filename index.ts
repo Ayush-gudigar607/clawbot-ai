e6f9744d7093d4ef440bd3249808d6da41a86f39
unknown <23ai009@sitmng.ac.in>
2026-09-12 21:59:45 +0530

    add the clawbot-ai doc in the index.ts

--- index.ts
+++ index.ts
@@ -5,7 +5,7 @@ const program=new Command();
 
 program.name("clawbot-ai").description("A CLI tool for Clawbot AI").version("0.1.0");
 
-
+//program to run the wakeup using the wakeup build in function
 program.command("wakeup").description("Wake up the Clawbot AI").action(async ()=>{
    await runwakeup();
 });
