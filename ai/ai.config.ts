7ac21a5e447273ffcf2ea60713d51b076a33d15c
Ayush <23ai009@sitmng.ac.in>
2026-09-14 19:19:20 +0530

    Update ai.config.ts

--- ai/ai.config.ts
+++ ai/ai.config.ts
@@ -1,11 +1,13 @@
 import { createOpenRouter } from "@openrouter/ai-sdk-provider";
 
+//getmodel function to get the agent using apikey 
 export function getAgentModel()
 {
     const provider=createOpenRouter({
         apiKey:process.env.OPENROUTER_API_KEY
     });
 
+//get the default model using openrouter
     const modelId=process.env.OPENROUTER_DEFAULT_MODEL;
 
     if(!modelId)
